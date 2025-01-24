dfced28e19cc0f26cbebcc8627b827d1f190b0c4
0187773933 <collincerbus@student.olympic.edu>
2025-01-24 09:46:34 -0500

    4

--- v1/routes/user.go
+++ v1/routes/user.go
@@ -1,6 +1,12 @@
 package routes
 
 import (
+	"fmt"
+	hex "encoding/hex"
+	json "encoding/json"
+	base64 "encoding/base64"
+	bolt "github.com/boltdb/bolt"
+	secretbox "golang.org/x/crypto/nacl/secretbox"
 	fiber "github.com/gofiber/fiber/v2"
 	user "github.com/0187773933/CheckInServer/v1/user"
 	server "github.com/0187773933/GO_SERVER/v1/server"
@@ -15,9 +21,20 @@ func UserNewForm( s *server.Server ) fiber.Handler {
 }
 
 func UserBlank( s *server.Server ) fiber.Handler {
-	temp_key := encryption.SecretBoxGenerateRandomKey()
-	temp_user := user.New()
 	return func( c *fiber.Ctx ) error {
+		temp_key := encryption.SecretBoxGenerateRandomKey()
+		temp_user := user.New()
+		db_result := s.DB.Update( func( tx *bolt.Tx ) error {
+			users_blank_bucket , _ := tx.CreateBucketIfNotExists( []byte( "users-blank" ) )
+			users_blank_bucket.Put( []byte( temp_user.UUID ) , temp_key[ : ] )
+			return nil
+		})
+		if db_result != nil {
+			fmt.Println( "Error saving blank user:", db_result )
+			return c.Status( fiber.StatusInternalServerError ).JSON(fiber.Map{
+				"error": "Failed to save blank user",
+			})
+		}
 		return c.JSON( fiber.Map{
 			"blank": temp_user ,
 			"ecb": temp_key ,
@@ -27,8 +44,73 @@ func UserBlank( s *server.Server ) fiber.Handler {
 
 func UserEdit( s *server.Server ) fiber.Handler {
 	return func( c *fiber.Ctx ) error {
+
+		body := c.Body()
+		// fmt.Println( "Raw Request Body:" , string( body ) )
+		var response map[string]interface{}
+		if err := json.Unmarshal( body , &response ); err != nil {
+			fmt.Println( "Error parsing JSON:", err)
+			return c.Status( fiber.StatusBadRequest ).JSON(fiber.Map{
+				"error": "Failed to parse JSON request",
+			})
+		}
+		encrypted_user_b64_string , ok := response[ "w" ].( string )
+		if !ok {
+			fmt.Println( "Error parsing JSON: w" , ok )
+			return c.Status( fiber.StatusBadRequest ).JSON(fiber.Map{
+				"error": "Failed to parse JSON request",
+			})
+		}
+		user_uuid , ok := response[ "u" ].( string )
+		if !ok {
+			fmt.Println( "Error parsing JSON: u" , ok )
+			return c.Status( fiber.StatusBadRequest ).JSON(fiber.Map{
+				"error": "Failed to parse JSON request",
+			})
+		}
+
+		second_key_string , ok := response[ "s" ].( string )
+		if !ok {
+			fmt.Println( "Error parsing JSON: u" , ok )
+			return c.Status( fiber.StatusBadRequest ).JSON(fiber.Map{
+				"error": "Failed to parse JSON request",
+			})
+		}
+		second_key_hex , _ := hex.DecodeString( second_key_string )
+		var second_key_bytes [32]byte
+		copy( second_key_bytes[ : ] , second_key_hex )
+		var inner_nonce [24]byte
+		copy( inner_nonce[ : ] , outer_decrypted[ 0 : 24 ] )
+
+		encrypted_user , _ := base64.StdEncoding.DecodeString( encrypted_user_b64_string )
+
+		var decrypted_user user.User
+		db_result := s.DB.View( func( tx *bolt.Tx ) error {
+			users_blank_bucket := tx.Bucket( []byte( "users-blank" ) )
+			outer_key := users_blank_bucket.Get( []byte( user_uuid ) )
+
+			var outer_key_array [32]byte
+			copy( outer_key_array[ : ] , outer_key )
+			var nonce [24]byte
+			copy( nonce[ : ] , encrypted_user[ 0 : 24 ] )
+			outer_decrypted , ok := secretbox.Open( nil , encrypted_user[ 24 : ] , &nonce , &outer_key_array )
+			if ok != true { fmt.Println( "Error decrypting user:" ) }
+
+			decrypted , ok := secretbox.Open( nil , outer_decrypted[ 24 : ] , &inner_nonce , &second_key_bytes )
+			if ok != true { fmt.Println( "Error decrypting user:" ) }
+
+			json.Unmarshal( decrypted , &decrypted_user )
+			return nil
+		})
+		fmt.Println( decrypted_user )
+		if db_result != nil {
+			fmt.Println( "Error saving blank user:", db_result )
+			return c.Status( fiber.StatusInternalServerError ).JSON(fiber.Map{
+				"error": "Failed to save blank user",
+			})
+		}
 		return c.JSON( fiber.Map{
-			"blank": user.New() ,
+			"blank": "" ,
 		})
 	}
 }
